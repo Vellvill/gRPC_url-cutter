0fa346753d7b24e4304162096b8fe87bb5580c87
unknown <gerasimoff.timofeii@yandex.ru>
2022-05-04 05:47:05 +0300

    refactor

--- internal/service/service.go
+++
@@ -1,8 +0,0 @@
-package service
-
-import "gRPC_cutter/internal/usecases"
-
-//Implementation структуры с набором используемых в usecases методов для использования в рамках сервиса.
-type Implementation struct {
-	repo usecases.Repository
-}
